f8001fb799a85904a9a03300e8dca638e3e2f6d4
jgunter7 <jason_gunter@hotmail.com>
2016-11-13 14:02:24 -0500

    Added more of the scene. Added Player script. Failed to get gun animations working.

---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+    //PUBLIC VARIABLES
+
+    //PRIVATE VARIABLES
+    private Transform _transform;
+	// Use this for initialization
+	void Start () {
+        this._transform = this.GetComponent<Transform>();
+	}
+	
+	// FixedUpdate is called once per frame
+	void FixedUpdate () {
+
+	}
+}
