33fd72535a371d98c83d3a83dd83114c8457c6e6
digma89 <digma89@hotmail.com>
2016-11-16 18:30:28 -0500

    gun shoot added sound and food

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -3,16 +3,51 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
     //PUBLIC VARIABLES
+    public Transform FlashPoint;
+    public GameObject MuzzleFlash;
+    public AudioSource MachineGunSound;
+    public AudioSource Eating;
+    public Transform PlayerCam;
 
     //PRIVATE VARIABLES
-    private Transform _transform;
+    
+    //private Transform _transform;
 	// Use this for initialization
 	void Start () {
-        this._transform = this.GetComponent<Transform>();
+        this.PlayerCam = this.GetComponent<Transform>();
 	}
 	
 	// FixedUpdate is called once per frame
 	void FixedUpdate () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            //Show MuzzleFlash at the flash point            
+            GameObject childObject = Instantiate(this.MuzzleFlash, this.FlashPoint.position,Quaternion.identity) as GameObject;
+            childObject.transform.parent = FlashPoint.transform; //put the MuzzleFlash inside Flashpoint
+            Destroy(childObject, 1);
+
+            RaycastHit hit;
+
+            if(Physics.Raycast (this.PlayerCam.position,this.PlayerCam.forward,out hit))
+            {
+                Debug.Log(hit.transform.gameObject);
+            }
+
+
+            //Play rifle sound
+            this.MachineGunSound.Play();
+        }
 
 	}
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+   
+        if (hit.gameObject.CompareTag("Food"))
+        {
+            Eating.Play();
+            Debug.Log("Food !!");
+            Destroy(hit.gameObject);
+        }
+    }
 }
